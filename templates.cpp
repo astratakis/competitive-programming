f6fd816ba206c5bc6e40fa8da53499b0581e7208
Andreas Stratakis <57875212+astratakis@users.noreply.github.com>
2021-11-03 23:53:02 +0200

    Created initial templates

---
+++ templates.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    return 0;
+}
