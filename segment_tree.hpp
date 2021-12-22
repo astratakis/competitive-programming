eb250595ba2782cb96e852c14c810449ebab522d
Andreas Stratakis <57875212+astratakis@users.noreply.github.com>
2021-12-22 13:42:26 +0200

    Moved final cpp file in folder export

--- segment_tree.hpp
+++ segment_tree.hpp
@@ -6,19 +6,20 @@
 
 #endif
 
+#define __generic__ template <typename T>
+
 #ifndef __SEGMENT_TREE__
 #define __SEGMENT_TREE__
-
-#define __generic__ template <typename T>
 
 template <typename T> class segment_tree {
     
     public:
-        segment_tree(const vector<T> __list) {
-            cout << "Hello";
-        }
+        segment_tree(const vector<T> __list);
 };
 
+__generic__ segment_tree<T>::segment_tree(const vector<T> __list) {
+    cout << "Hello from constructor\n";
+}
 
 
 #endif
